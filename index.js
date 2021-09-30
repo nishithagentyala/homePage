7445476702cac4e495be2fe36dce2cb361164345
nishithagentyala <nishithagentyala@gmail.com>
2021-09-30 22:19:06 +0530

    final commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const loc = document.querySelector(".loc");
 const nav = document.querySelector("nav");
 const bar = document.querySelector(".bar");
 const navbar = document.querySelector(".nav-bar");
+const close = document.querySelector(".close");
 function round() {
   btn.style.left = "0px";
   car.innerHTML = "PICK ZOOMCAR FROM";
@@ -29,7 +30,12 @@ function oneway() {
   loc.placeholder = "";
 }
 const showBar = () => {
-  navbar.classList.toggle("show");
-  bar.classList.toggle("hide");
+  navbar.classList.add("show");
+  bar.classList.add("hide");
+};
+const closeBar = () => {
+  navbar.classList.remove("show");
+  bar.classList.remove("hide");
 };
 bar.addEventListener("click", showBar);
+close.addEventListener("click", closeBar);
